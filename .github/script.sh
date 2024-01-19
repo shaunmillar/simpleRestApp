08ae5b0290aeebbd4aba1bd0b4380396a669d063
shaunmillar <shaunmillaris@gmail.com>
2024-01-19 10:09:03 -0800

    Update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -6,8 +6,11 @@ namespace=$3
 
 deploy=`oc get deployment $app_name`
 if [[ "$?" -eq 0 ]]; then
+    echo "1"
     oc set image deployment/$app_name $app_name=$image
+    echo "2"
     oc rollout restart deployment/$app_name
+    echo "3"
 else
     oc new-app $image --name $app_name -n $namespace
 fi
